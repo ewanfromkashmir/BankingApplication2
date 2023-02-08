c9f25341245de95bb9906cb748195306a573601d
ewanfromkashmir <ewanhughes26@gmail.com>
2023-02-08 16:54:00 +0000

    Added InputObtainer class with methods for obtaining user input of specific types

---
+++ AcmeBanking/InputObtainer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeBanking
+{
+    public class InputObtainer
+    {
+        private static string ObtainUserInput()
+        {
+            string? input = Console.ReadLine();
+
+            while (input == null)
+            {
+                Display.DisplayWarning("No input. Please try again.");
+                input = Console.ReadLine();
+            }
+
+            return input;
+        }
+
+        public static double ObtainInputDouble()
+        {
+            double inputDouble;
+
+            while (!double.TryParse(ObtainUserInput(), out inputDouble))
+            {
+                Display.DisplayWarning("Unrecognised input. Please try again.");
+            }
+
+            return inputDouble;
+        }
+
+        public static int ObtainInputInteger()
+        {
+            int inputInteger;
+
+            while (!int.TryParse(ObtainUserInput(), out inputInteger))
+            {
+                Display.DisplayWarning("Unrecognised input. Please try again.");
+            }
+
+            return inputInteger;
+        }
+
+        public static int ObtainMenuSelection(int options)
+        {
+            int menuSelection;
+
+            while (!(int.TryParse(ObtainUserInput(), out menuSelection) && menuSelection > 0 && menuSelection <= options))
+            {
+                Display.DisplayWarning("Unrecognised input. Please try again.");
+            }
+
+            return menuSelection;
+        }
+    }
+}
