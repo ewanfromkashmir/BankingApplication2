848634a333da43be2b0308a67c6f0ef2df30051b
ewanfromkashmir <ewanhughes26@gmail.com>
2023-02-08 20:40:58 +0000

    Implemented main menu and skeleton for level two menus

---
+++ AcmeBanking/Menus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeBanking
+{
+    public class Menus
+    {
+        public static void MainMenu()
+        {
+            Display.DisplayMessage("Welcome to Acme Banking Solutions.");
+            Display.DisplayPrompt("Does the customer currently have an account with us? \n1. Yes \n2. No\n\n");
+
+            switch (InputObtainer.ObtainMenuSelection(2))
+            {
+                case 1: { ExistingCustomersMenu(); break; }
+                case 2: { NewCustomersMenu(); break; }
+            }
+        }
+
+        private static void ExistingCustomersMenu()
+        {
+            Display.DisplayPrompt("Would the customer like to access an existing account or open a new account? \n1. Access existing account \n2. Open new account\n\n");
+
+            switch (InputObtainer.ObtainMenuSelection(2))
+            {
+                case 1: { break; }
+                case 2: { break; }
+            }
+
+        }
+
+        private static void NewCustomersMenu()
+        {
+
+        }
+    }
+}
