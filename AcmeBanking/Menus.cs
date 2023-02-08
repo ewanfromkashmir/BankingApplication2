ffe4df19b7d9b017947134e4dd4fac852f2fea59
ewanfromkashmir <ewanhughes26@gmail.com>
2023-02-08 21:09:02 +0000

    Added framework for some level 3 menus (AccountCreationMenu and ExistingAccountsMenu)

--- AcmeBanking/Menus.cs
+++ AcmeBanking/Menus.cs
@@ -13,10 +13,10 @@ namespace AcmeBanking
             Display.DisplayMessage("Welcome to Acme Banking Solutions.");
             Display.DisplayPrompt("Does the customer currently have an account with us? \n1. Yes \n2. No\n\n");
 
-            switch (InputObtainer.ObtainMenuSelection(2))
+            switch(InputObtainer.ObtainMenuSelection(2))
             {
                 case 1: { ExistingCustomersMenu(); break; }
-                case 2: { NewCustomersMenu(); break; }
+                case 2: { AccountCreationMenu(); break; }
             }
         }
 
@@ -24,17 +24,32 @@ namespace AcmeBanking
         {
             Display.DisplayPrompt("Would the customer like to access an existing account or open a new account? \n1. Access existing account \n2. Open new account\n\n");
 
-            switch (InputObtainer.ObtainMenuSelection(2))
+            switch(InputObtainer.ObtainMenuSelection(2))
             {
-                case 1: { break; }
-                case 2: { break; }
+                case 1: { ExistingAccountsMenu(); break; }
+                case 2: { AccountCreationMenu(); break; }
             }
 
         }
 
-        private static void NewCustomersMenu()
+        private static void AccountCreationMenu()
         {
+            Display.DisplayPrompt("What type of the account would the customer like to create? \n1. Personal account \n2. Business account \n3. ISA account\n\n");
+
+            switch (InputObtainer.ObtainMenuSelection(3))
+            {
+                case 1: { break; }
+                case 2: { break; }
+                case 3: { break; }
+            }
 
         }
+
+        private static void ExistingAccountsMenu()
+        {
+            Display.DisplayPrompt("Enter customer account number: ");
+        }
+
+        
     }
 }
