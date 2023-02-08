3cf17c8d88d386ef3b59bfd8aadbe0c8155e224b
ewanfromkashmir <ewanhughes26@gmail.com>
2023-02-08 20:49:43 +0000

    Added ObtainInputName method

--- AcmeBanking/InputObtainer.cs
+++ AcmeBanking/InputObtainer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AcmeBanking
@@ -43,6 +44,19 @@ namespace AcmeBanking
             }
 
             return inputInteger;
+        }
+
+        public static string ObtainInputName()
+        {
+            string name = ObtainUserInput();
+
+            while(!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+            {
+                Display.DisplayWarning("Unsupported input. Please try again.");
+                name = ObtainUserInput();
+            }
+
+            return name;
         }
 
         public static int ObtainMenuSelection(int options)
