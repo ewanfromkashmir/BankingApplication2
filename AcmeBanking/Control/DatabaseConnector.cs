c4aa07cb305873f338d8145f9dca04785c995bc7
ewanfromkashmir <ewanhughes26@gmail.com>
2023-02-11 19:24:08 +0000

    Created DatabaseConnector class with implemented ConnectToDatabase method

---
+++ AcmeBanking/Control/DatabaseConnector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeBanking.Control
+{
+    class DatabaseConnector
+    {
+        public static SqlConnection ConnectToDatabase(string connectionString)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
